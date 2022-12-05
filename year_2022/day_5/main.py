746cd43b0ece9cfb3287a7ff011295cf1a4c4643
Long Nguyen <nguyenlonguhs@gmail.com>
2022-12-05 10:55:42 -0800

    added day 5

---
+++ year_2022/day_5/main.py
@@ -0,0 +1,36 @@
+with open('test.txt', 'r') as input_file:
+    input_text = input_file.read()
+initial_state = input_text.split('\n\n')[0].splitlines()
+instructions = input_text.split('\n\n')[1].splitlines()
+
+board_state = {}
+
+stack_indices = []
+crate_positions = []                
+crates_line = initial_state[-1]
+
+for i in range (len(crates_line)):
+    if crates_line[i].isdigit():
+        stack_indices.append(i)
+        crate_positions.append(crates_line[i])
+
+for crate_position in crate_positions:
+    board_state[crate_position] = []
+
+initial_state.pop() 
+for line in reversed(initial_state):
+    for stack_index, crate_position in zip(stack_indices, crate_positions):
+        if line[stack_index].isalpha():
+            board_state[crate_position].append(line[stack_index])
+
+for line in instructions:
+    instruction = line.split()
+    instruction_n = []
+    for s in instruction:
+        if s.isdigit():
+            instruction_n.append(int(s))
+            
+
+    
+
+print(instruction_digits)
