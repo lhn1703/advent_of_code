e33bb52ea23de3463e16fab32da6923c2d7ccde1
lhn1703 <55109682+lhn1703@users.noreply.github.com>
2022-12-16 23:52:16 -0800

    stupid monkeys

--- year_2022/day_11/main.py
+++ year_2022/day_11/main.py
@@ -1,12 +1,17 @@
 with open('test.txt', 'r') as input_file:
     input_text = input_file.read().splitlines()
 
+# // is integer division operator
+
 class Monkey:
-    def __init__(self, starting_items = None, operation = None, result = None, modulo = None, true_throw = None, false_throw = None) -> None:
+    def __init__(self, starting_items = None, operation = None, modulo = None, true_throw = None, false_throw = None, worry_result = None) -> None:
         self.starting_items = starting_items
-        self.operation = operation
-        self.result = result
+        self.operation = operation                   
         self.modulo = modulo
         self.true_throw = true_throw
         self.false_throw = false_throw
+        self.worry_result = worry_result            # worry result will be // by 3 after every operation
+
+
+    
         
