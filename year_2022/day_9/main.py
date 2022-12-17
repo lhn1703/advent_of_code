31724282399333926ef4baae75f0cba0af62bce3
lhn1703 <55109682+lhn1703@users.noreply.github.com>
2022-12-16 20:35:01 -0800

    Update main.py

--- year_2022/day_9/main.py
+++ year_2022/day_9/main.py
@@ -1,15 +1,15 @@
-with open('test.txt', 'r') as input_file:
+with open('input.txt', 'r') as input_file:
     input_text = input_file.read().splitlines()
 
 def move_head(h_coord, direction):
     hx, hy = h_coord
-    if direction == 'U':
+    if direction == 'R':
         hx += 1
-    elif direction == 'D':
-        hx -= 1
     elif direction == 'L':
+        hx -= 1
+    elif direction == 'D':
         hy -= 1
-    elif direction == 'R':
+    elif direction == 'U':
         hy += 1
     else:
         raise Exception(direction, 'is not a valid direction')
@@ -33,31 +33,30 @@ def move_tail(new_h_coord, t_coord, direction):            # can only move 1 til
         (hx+1, hy+1)
     )
 
+    tail_diagonals = [
+        (tx-1, ty+1),
+        (tx+1, ty+1),
+        (tx-1, ty-1),
+        (tx+1, ty-1)
+    ]
+
     if t_coord in adjacent_coordinates:
         pass
-    elif hx == tx or hy == ty:          # if the head and tail are in the same row or column, simple translation
-        if direction == 'U':
-            tx += 1
-        elif direction == 'D':
-            tx -= 1
-        elif direction == 'L':
+    elif hx == tx:                                      # check if head is same row/column as tail and find the nearest adjacent point
+        if (tx, ty-1) in adjacent_coordinates:
             ty -= 1
-        elif direction == 'R':
+        else:
             ty += 1
-    else:
-        if direction == 'U':            # if vertical direction, then must be in the same column
-            tx += 1
-            ty = hy
-        elif direction == 'D':
+    elif hy == ty:
+        if (tx-1, ty) in adjacent_coordinates:
             tx -= 1
-            ty = hy
-        elif direction == 'L':          # if horizontal movement, must be in the same row
-            ty -= 1
-            tx = hx
-        elif direction == 'R':
-            ty += 1
-            tx = hx
-    
+        else:
+            tx += 1
+    else:                                               # move tail by one diagonal adjacent to head
+        for diagonal in tail_diagonals:
+            if diagonal in adjacent_coordinates:
+                tx, ty = diagonal
+        
     new_t_coord = (tx, ty)
 
     return new_t_coord
@@ -88,18 +87,12 @@ unique_t_coords = [(0,0)]
 for line in input_text:
     direction = line.split()[0]
     steps = int(line.split()[1])
-
     for step in range(steps):
         rope[0] = move_head(rope[0], direction)
         for i in range(len(rope)-1):
             rel_h_coord = rope[i]
-            
             rel_t_coord = rope[i+1]
             rope[i+1] = move_tail(rel_h_coord, rel_t_coord, direction)
-
-            print('direction:', direction, ',step:', step, ',i:', i, rope)
-            input("Press Enter to continue...")
         if rope[9] not in unique_t_coords:
             unique_t_coords.append(rope[9])
-
 print('part2:', len(unique_t_coords))
