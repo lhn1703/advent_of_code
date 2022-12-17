e9aac20d2b1792af43cdaf4b5488262e76d450ad
lhn1703 <55109682+lhn1703@users.noreply.github.com>
2022-12-16 23:09:02 -0800

    PAIN

--- year_2022/day_10/main.py
+++ year_2022/day_10/main.py
@@ -1,4 +1,4 @@
-with open('test.txt', 'r') as input_file:
+with open('input.txt', 'r') as input_file:
     input_text = input_file.read().splitlines()
 
 def parse_line(line):
@@ -22,31 +22,101 @@ while instruction_program != []:
     current_instruction = instruction_program.pop(0)
     op, value = current_instruction
 
-    current_cycle += 1
+    # print(current_cycle, rx)
+
+
     if current_cycle in cycle_check:
         sum += rx * current_cycle   
-        print('sum', current_cycle, rx, rx*current_cycle) 
+        # print('sum', current_cycle, rx, rx*current_cycle) 
 
     if op == 'noop':
-        pass
+        current_cycle += 1
     elif op == 'addx':
         current_cycle += 1
-        rx += value
-        print(current_cycle, rx)
         if current_cycle in cycle_check:
             sum += rx * current_cycle   
-            print('sum', current_cycle, rx, rx*current_cycle) 
+            # print('sum', current_cycle, rx, rx*current_cycle) 
 
-    print(current_cycle, rx)
-
+        # print(current_cycle, rx)
+        current_cycle += 1
+        rx += value
+        # print(current_cycle, rx)
+        
 print('part1', sum)
 
+current_cycle = 1
+rx = 1
+sprite_position = []
+crt = ["." for i in range(40)]
 
-# current_cycle = 1
-# rx = 1
-# line = ['........................................']
+instruction_program = []
+for line in input_text:
+    op, value = parse_line(line)
+    instruction = (op, value)
+    instruction_program.append(instruction)
+
+while instruction_program != []:
+    current_instruction = instruction_program.pop(0)
+    op, value = current_instruction
+
+    if rx == -1:
+        sprite_position = [0]
+    elif rx == 0:
+        sprite_position = [0,1]
+    elif rx == 40:
+        sprite_position = [39]
+    elif rx == 39:
+        sprite_position = [38, 39]
+    else:
+        sprite_position = [rx-1, rx, rx+1]
+
+    # for i in range(len(crt)):
+    #     if crt[i] == 's' or crt[i] == 'o':
+    #         crt[i] = '.'
+
+    # for s in sprite_position:
+    #     crt[s] = 's'
+    # crt[current_cycle-1] = 'o'
 
 
 
-#     print(current_cycle, rx)
+    if current_cycle-1 in sprite_position:
+        crt[current_cycle-1] = '#'
+    if current_cycle % 40 == 0:
+        current_cycle -= 40
+        print(''.join(crt))
+        crt = ["." for i in range(40)]
+
+    # print('cycle:', current_cycle, op, value, sprite_position)
+    # print(''.join(crt))
+    # input('outside loop\n')
+
+    if op == 'noop':
+        current_cycle += 1
+    elif op == 'addx':
+        current_cycle += 1
+
+        # for i in range(len(crt)):
+        #     if crt[i] == 'o':
+        #         crt[i] = '.'
+        # crt[current_cycle-1] = 'o'
+
+        if current_cycle-1 in sprite_position:
+            crt[current_cycle-1] = '#'
+        if current_cycle % 40 == 0:
+            current_cycle -= 40
+            print(''.join(crt)) 
+            crt = ["." for i in range(40)]
+
+        # print('cycle:', current_cycle, op, value, sprite_position)
+        # print(''.join(crt))
+        # input('second addx\n')
+
+        
+        current_cycle += 1
+        rx += value
+
+
+        
+        
 
