c4d08429860fbbcf825220f33b41479704061b3a
lhn1703 <55109682+lhn1703@users.noreply.github.com>
2022-12-21 21:21:52 -0800

    Update main.py

--- year_2022/day_15/main.py
+++ year_2022/day_15/main.py
@@ -1,11 +1,10 @@
 import parse
-from tqdm import tqdm
-import time
+from itertools import combinations
 
-with open('input.txt', 'r') as input_file:
+with open('test.txt', 'r') as input_file:
     input_text = input_file.read().splitlines()
 
-y_pos = 2000000
+y_pos = 10
 
 min_x = None
 max_x = None
@@ -25,6 +24,7 @@ class Sensor:
         self.beacon_x = beacon_x
         self.beacon_y = beacon_y
         self.manhattan_distance = manhattan_distance
+    perimeter = None
 
 # first pass to map the initial board
 for line in input_text:
@@ -48,24 +48,65 @@ for line in input_text:
     if max_y == None or sensor_y + manhattan_distance > max_y:
         max_y = sensor_y + manhattan_distance
 
-start_time = time.time()
+
 
 sum = 0
 for x in range(min_x, max_x + 1):
     current_coord = (x, y_pos)
+    if current_coord in map.keys():
+        continue
     
     for sensor in sensor_list:
-        if current_coord in map.keys():
-            continue
+        
         sensor_x = sensor.sensor_x
         sensor_y = sensor.sensor_y
         current_mhd = mhd(x, y_pos, sensor_x, sensor_y)
         if current_mhd <= sensor.manhattan_distance:
             map[current_coord] = '#'
             sum += 1
+            continue
 
-print('part1', sum, 'time:', time.time() - start_time)
+print('part1', sum)
 
+### PART 2 ALGORITHM ###
+# solution from https://www.youtube.com/watch?v=w7m48_uCvWI 
+# each sensor has a manhattan perimiter that is diamond shaped
+# y - y0 = m(x - x0) + d
+# m = ± 1
+# d = ± manhattan distance
+# at sensor location (x0, y0)
+# positive line eqn: y = x + (-x0 + y0 ± d)
+# negative line eqn: y = -x + (x0 + y0 ± d)
+# find the points where a pair of positive lines have a gap of 2
+    # out of those points find the point where the a pair of negative lines have a gap of 2
+        # verify that this point is not wihtin manhattan distance of any sensor
 
+negative_line_intercepts = []
+positive_line_intercepts = []
 
+for line in input_text:
+    sensor_x, sensor_y, beacon_x, beacon_y = parse.parse('Sensor at x={}, y={}: closest beacon is at x={}, y={}', line)
+    sensor_x, sensor_y, beacon_x, beacon_y = int(sensor_x), int(sensor_y), int(beacon_x), int(beacon_y)
+    manhattan_distance = mhd(sensor_x, sensor_y, beacon_x, beacon_y)
+
+    positive_line_intercepts.append(-sensor_x + sensor_y + manhattan_distance)
+    positive_line_intercepts.append(-sensor_x + sensor_y - manhattan_distance)
+    negative_line_intercepts.append(sensor_x + sensor_y + manhattan_distance)
+    negative_line_intercepts.append(sensor_x + sensor_y - manhattan_distance)
+
+neg_intercept_combinations = combinations(negative_line_intercepts, 2)
+pos_intercept_combinations = combinations(positive_line_intercepts, 2)
+
+pos_intercept_candidates = []
+neg_intercept_candidates = []
+
+# the solution must be in between these intercept pairs
+for combo in pos_intercept_combinations:
+    intercept0, intercept1 = combo
+    if abs(intercept0 - intercept1) == 2:
+        pos_intercept_candidates.append(combo)
+for combo in neg_intercept_combinations:
+    intercept0, intercept1 = combo
+    if abs(intercept0 - intercept1) == 2:
+        neg_intercept_candidates.append(combo)
 
