68a932a6bb432dc45fcb292891841910962df2f2
lhn1703 <55109682+lhn1703@users.noreply.github.com>
2022-12-21 22:33:58 -0800

    meh

--- year_2022/day_15/main.py
+++ year_2022/day_15/main.py
@@ -1,9 +1,10 @@
 import parse
-from itertools import combinations
+from itertools import combinations, permutations
 
 with open('test.txt', 'r') as input_file:
     input_text = input_file.read().splitlines()
 
+boundary = 20
 y_pos = 10
 
 min_x = None
@@ -47,21 +48,21 @@ for line in input_text:
     if max_y == None or sensor_y + manhattan_distance > max_y:
         max_y = sensor_y + manhattan_distance
 
-sum = 0
-for x in range(min_x, max_x + 1):
-    current_coord = (x, y_pos)
+# sum = 0
+# for x in range(min_x, max_x + 1):
+#     current_coord = (x, y_pos)
     
-    for sensor in sensor_list:
-        if current_coord in map.keys():
-            continue
-        sensor_x = sensor.sensor_x
-        sensor_y = sensor.sensor_y
-        current_mhd = mhd(x, y_pos, sensor_x, sensor_y)
-        if current_mhd <= sensor.manhattan_distance:
-            map[current_coord] = '#'
-            sum += 1
+#     for sensor in sensor_list:
+#         if current_coord in map.keys():
+#             continue
+#         sensor_x = sensor.sensor_x
+#         sensor_y = sensor.sensor_y
+#         current_mhd = mhd(x, y_pos, sensor_x, sensor_y)
+#         if current_mhd <= sensor.manhattan_distance:
+#             map[current_coord] = '#'
+#             sum += 1
 
-print('part1', sum)
+# print('part1', sum)
 
 
 ### PART 2 ALGORITHM ###
@@ -90,8 +91,32 @@ for line in input_text:
     negative_line_intercepts.append(sensor_x + sensor_y + manhattan_distance)
     negative_line_intercepts.append(sensor_x + sensor_y - manhattan_distance)
 
-neg_intercept_combinations = combinations(negative_line_intercepts, 2)
-pos_intercept_combinations = combinations(positive_line_intercepts, 2)
+    print('sensor ({},{}), d={}, pos={},{}; neg={},{}'.format(sensor_x, sensor_y, manhattan_distance, -sensor_x + sensor_y + manhattan_distance, -sensor_x + sensor_y - manhattan_distance, sensor_x + sensor_y + manhattan_distance, sensor_x + sensor_y - manhattan_distance))
+
+positive_line_intercepts = list(set(positive_line_intercepts))
+negative_line_intercepts = list(set(negative_line_intercepts))
+
+p_temp = []
+n_temp = []
+for i in range(len(positive_line_intercepts)):
+    if 0 <= positive_line_intercepts[i] <= boundary:
+        p_temp.append(positive_line_intercepts[i])
+for i in range(len(negative_line_intercepts)):
+    if 0 <= negative_line_intercepts[i] <= boundary:
+        n_temp.append(negative_line_intercepts[i])
+
+positive_line_intercepts = p_temp
+negative_line_intercepts = n_temp
+
+
+print(sorted(positive_line_intercepts))
+print(sorted(negative_line_intercepts))
+
+neg_intercept_combinations = list(combinations(negative_line_intercepts, 2))
+pos_intercept_combinations = list(combinations(positive_line_intercepts, 2))
+
+# print(pos_intercept_combinations)
+# print(neg_intercept_combinations)
 
 pos_intercept_candidates = []
 neg_intercept_candidates = []
@@ -105,4 +130,37 @@ for combo in neg_intercept_combinations:
     intercept0, intercept1 = combo
     if abs(intercept0 - intercept1) == 2:
         neg_intercept_candidates.append(combo)
+
+# the point must be vertical midpoint between two positive intercept candidate pairs
+# the point must be the horizontal midpoint between two negative intercept candidate pairs
+
+
+beacon_candidates = []
+for pos_combo in pos_intercept_candidates:
+    pos1, pos2 = pos_combo
+    x = (pos1 + pos2) / 2
+    for neg_combo in neg_intercept_candidates:
+        neg1, neg2 = neg_combo
+        y = (neg1 + neg2) / 2
+        beacon_candidates.append((x,y))
+
+beacon_candidates = list(set(beacon_candidates))
+
+for b in beacon_candidates:
+    print(b)
+
+for beacon_candidate in beacon_candidates:
+    found = True
+    beacon_x, beacon_y = beacon_candidate
+    for sensor in sensor_list:
+        sensor_x = sensor.sensor_x
+        sensor_y = sensor.sensor_y
+        current_mhd = mhd(beacon_x, beacon_y, sensor_x, sensor_y)
+        if current_mhd <= sensor.manhattan_distance:
+            found = False
+            break
+    if found:
+        print('part2', beacon_candidate)
+        break
+            
 
